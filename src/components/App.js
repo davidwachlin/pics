dd212486263c61a72ca006bb5b43dbfb520109b4
David Wachlin <davidwachlin@gmail.com>
2020-02-03 16:45:12 -0500

    adds axios call

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
-
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit = term => {
-    console.log(term);
+  state = {
+    images: [],
   };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID 098ba34bf7a9e461a4fa9f9183af5cf31e200955d58c7e0612299db9a71c60f1',
+      },
+    });
+    console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images.
       </div>
     );
   }
